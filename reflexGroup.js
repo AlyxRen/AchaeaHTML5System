3b6af23617f6b8d2d0cb66a8b297fe85093e51b2
Stephen "Rixius" Middleton <stephen+git@rixi.us>
2014-02-07 15:49:37 -0600

    Add Actions for Triggers, and empty other Objects

--- reflexGroup.js
+++ reflexGroup.js
@@ -3,8 +3,22 @@ var _slice = [].slice,
     Base,
     Group,
     ReflexPackage,
+
     Reflex,
+    Alias,
+    Keybind,
+    Trigger,
     Code,
+
+    Action,
+    Command,
+    Notice,
+    Gag,
+    Highlight,
+    Rewrite,
+    CallCode,
+    ExecCode,
+
     ReflexHelpers
 
 Base = Class.extend({
@@ -33,7 +47,7 @@ Group = Base.extend({
             throw 'input not instanceof Base'
         }
         this.items.push(action);
-        return this;
+        return this
     }
 })
 ReflexPackage = Group.extend({
@@ -47,13 +61,122 @@ Reflex = Base.extend({
     init: function(id, type, name, enabled) {
         this._super(id, type, name, enabled)
         this.actions = []
+    },
+    act: function(action) {
+        if (!(action instanceof Action)) {
+            throw 'Action not instanceof Action'
+        }
+        this.actions.push(action)
+        return this
     }
+})
+
+Alias = Reflex.extend({
+    init: function(id, name, text, matching, whole_words,
+                   presuf, enabled) {
+        this._super(id, 'alias', name, enabled)
+        this.text = text
+        this.matching = matching
+        this.whole_words = whole_words
+        this.prefix_suffix = presuf
+    },
+    act: function(action) {
+        if (!(action instanceof Command)) {
+            throw 'Can only add Commands'
+        }
+        return this._super(action)
+    }
+})
+
+Keybind = Reflex.extend({
+    init: function(id, name, key, key_alt, key_ctrl, key_shift, enabled) {
+        this._super(id, 'keybind', name, enabled)
+        this.key = key
+        this.key_alt   = !!key_alt
+        this.key_ctrl  = !!key_ctrl
+        this.key_shift = !!key_shift
+    },
+    act: function(action) {
+        if (!(action instanceof Command)) {
+            throw 'Can only add Commands'
+        }
+        return this._super(action)
+    }
+})
+
+Trigger = Reflex.extend({
+
 })
 
 Code = Reflex.extend({
     init: function(id, name, code, enabled) {
         this._super(id, 'function', name, enabled)
         this.code = code
+    },
+    act: function(a) { return this }
+})
+
+Action = Class.extend({
+
+})
+
+Command = Action.extend({
+    init: function(command, presuf) {
+        if (!typeof presuf == 'undefined') {
+            this.prefix_suffix = presuf
+        }
+        this.command = command
+    }
+})
+
+Notice = Action.extend({
+    'init': function(text, fg, bg) {
+        this.action = "notify"
+        this.notice = text
+        this.notice_fg = fg
+        this.notice_bg = bg
+    }
+})
+
+Gag = Action.extend({
+    'init': function() {
+        this.action = "gag"
+    }
+})
+
+Highlight = Action.extend({
+    'init': function(type, fg, bg, backref) {
+        this.action = "highlight"
+        this.highlight = type
+        this.highlight_backref = backref
+        this.highlight_fg = fg
+        this.highlight_bg = bg
+    }
+})
+
+Rewrite = Action.extend({
+    'init': function(type, text, colors, fg, bg, backref) {
+        this.action = "rewrite"
+        this.rewrite = type
+        this.rewrite_text = text
+        this.rewrite_backref = backref
+        this.rewrite_colors = colors
+        this.rewrite_fg = fg
+        this.rewrite_bg = bg
+    }
+})
+
+CallCode = Action.extend({
+    'init': function(fn) {
+        this.action = "function"
+        this.fn = fn
+    }
+})
+
+ExecCode = Action.extend({
+    'init': function(script) {
+        this.action = "script"
+        this.script = script
     }
 })
 
@@ -61,6 +184,19 @@ ReflexHelpers = {
     codeCompile: function() {
         var args = 1 <= arguments.length ? _slice.call(arguments, 0) : []
         return args.join('\n')
+    },
+    AliasMatching: {
+        contains: 'substring',
+        begins: 'begins',
+        exact: 'exact',
+        regex: 'regexp'
+    },
+    RewriteHighlightOptions: {
+        match: 'match',
+        line: 'line',
+        prefix: 'prefix',
+        suffix: 'suffix',
+        backref: 'backref'
     }
 }
 
@@ -70,9 +206,24 @@ ReflexHelpers = {
 exports.Base = Base
 exports.Group = Group
 exports.ReflexPackage = ReflexPackage
+
 exports.Reflex = Reflex
+exports.Alias = Alias
+exports.Keybind = Keybind
+exports.Trigger = Trigger
 exports.Code = Code
+
+exports.Action = Action
+exports.Command = Command
+exports.Notice = Notice
+exports.Gag = Gag
+exports.Highlight = Highlight
+exports.Rewrite = Rewrite
+exports.CallCode = CallCode
+exports.ExecCode = ExecCode
+
 exports.ReflexHelpers = ReflexHelpers
+
 exports.import = function Import(target) {
     for (key in this) {
         if (this[key] != Import) {
